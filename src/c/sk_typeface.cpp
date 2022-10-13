f8935e540c094a905b81145b8a7d652b556e53c7
John Carruthers <johnc@frag-labs.com>
2022-10-13 12:58:33 -0500

    Fix build.
    Remove the call to release on windows build, SkCreateTypefaceFromLOGFONT does it for us.

--- src/c/sk_typeface.cpp
+++ src/c/sk_typeface.cpp
@@ -31,7 +31,7 @@ sk_typeface_t* sk_typeface_create_from_ctfont(CTFontRef font) {
 
 #ifdef SK_BUILD_FOR_WIN
 sk_typeface_t* sk_typeface_create_from_logfont(LOGFONT font) {
-	return ToTypeface(SkCreateTypefaceFromLOGFONT(font).release());
+	return ToTypeface(SkCreateTypefaceFromLOGFONT(font));
 }
 #endif
 
